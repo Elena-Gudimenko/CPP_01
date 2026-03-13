c51b53bee099744fad13a43c84577e718c552711
Elena Gudimenko <goodislena@gmail.com>
2026-03-13 18:31:07 -0300

    ex00

---
+++ ex00/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../include/Zombie.hpp"
+
+int main(void) {
+    std::cout << "--Stack allocation (randomChump)--" << std::endl;
+    randomChump("StackZombie");
+
+    std::cout << "\n --Heap allocation (newZombie)--" << std::endl;
+    Zombie* heapZombie = newZombie("HeapZombie");
+    heapZombie->announce();
+    delete heapZombie;
+
+    return 0;
+}
