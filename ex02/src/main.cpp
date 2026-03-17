4f227cf38a24a89d00b8f8f2708dc2c67df3774a
Elena Gudimenko <goodislena@gmail.com>
2026-03-17 13:47:54 -0300

    ex03

---
+++ ex02/src/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+
+    std::cout << "Addresses: " << std::endl;
+    std::cout << "O endereço de memória da variável string: " << &str << std::endl;
+    std::cout << "O endereço de memória mantido por stringPTR: " << stringPTR << std::endl;
+    std::cout << "O endereço de memória mantido por stringREF: " << &stringREF << std::endl;
+
+    std::cout << "\nValues: " << std::endl;
+    std::cout << "O valor da variável str: " << str << std::endl;
+    std::cout << "O valor apontado por stringPTR: " << stringPTR << std::endl;
+    std::cout << "O valor apontado por stringREF: " << stringREF << std::endl;
+}
