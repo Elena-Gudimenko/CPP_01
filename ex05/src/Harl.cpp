b8a9f4c1f6fc5a0b4017598de53d72d3ac701ab0
Elena Gudimenko <goodislena@gmail.com>
2026-03-21 18:36:42 -0300

    final

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,32 +6,32 @@ Harl::Harl() {
 Harl::~Harl() {
 }
 
-void Harl::debug(void) {
+void Harl::_debug(void) {
     std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "Eu amo ter bacon extra para o meu hambúrguer 7XL-duplo-queijo-triplopicles-ketchup-especial. Eu realmente amo!" << std::endl;
 }
 
-void Harl::info(void) {
+void Harl::_info(void) {
     std::cout << "[ INFO ]" << std::endl;
     std::cout << "Eu não acredito que adicionar bacon extra custa mais dinheiro. Vocês não colocaram bacon suficiente no meu hambúrguer! Se vocês tivessem colocado, eu não estaria pedindo por mais!" << std::endl;
 }
 
-void Harl::warning(void) {
+void Harl::_warning(void) {
     std::cout << "[ WARNING ]" << std::endl;
     std::cout << "Eu acho que mereço ter bacon extra de graça. Eu venho aqui há anos, enquanto você começou a trabalhar aqui apenas no mês passado." << std::endl;
 }
 
-void Harl::error(void) {
+void Harl::_error(void) {
     std::cout << "[ ERROR ]" << std::endl;
     std::cout << "Isto é inaceitável! Eu quero falar com o gerente agora." << std::endl;
 }
 
 void Harl::complain(std::string level) {
     void (Harl::*funcs[])(void) = {
-        &Harl::debug,
-        &Harl::info,
-        &Harl::warning,
-        &Harl::error
+        &Harl::_debug,
+        &Harl::_info,
+        &Harl::_warning,
+        &Harl::_error
     };
     std::string levels[] = {
         "DEBUG",
